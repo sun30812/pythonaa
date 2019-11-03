5c3ec3d5e8358bf162df92df6c6f6d0caba87c41
sun30812 <sun30812@naver.com>
2019-11-03 09:31:46 +0000

    일부 내용 수정 및 업데이트

--- cpp/main2.cpp
+++ cpp/main2.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <syscall.h>
 
+int menu_num;
 // 함수 목록
 void menus();
 
 int main()
 {
-    enum menus {go, info, exit};
+  printf("사용을 환영합니다!\n\n");
+  menus();
 
-    printf("사용을 환영합니다!\n");
+  printf("사용해주셔서 감사합니다!!\n");
+
+  return 0;
+}
+
+void menus()
+{
+  enum menus { go=1, info, exit_code };
+  while (menu_num != exit_code) {
+  printf("메뉴를 선택해 주십시오\n");
     printf("1. 시작하기\n");
     printf("2. 정보\n");
     printf("3. 나가기\n");
+    printf("입력: "), scanf("%d", &menu_num);
+
+    system("clear");
+  }
 }
